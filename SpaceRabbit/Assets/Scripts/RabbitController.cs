ade51023853ebdca4923a5bb19e06314886c06d5
Veronica Hu <veronica.ss.hu@gmail.com>
2023-10-16 22:18:27 -0700

    implemented jumping
    Rabbit can jump left/right (no change in animation), background expanded, currently no collider object for the ground

--- SpaceRabbit/Assets/Scripts/RabbitController.cs
+++ SpaceRabbit/Assets/Scripts/RabbitController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class RabbitController : MonoBehaviour
 {
     Rigidbody2D rabbit;
-    public float jumpSpeed, maxSpeed;
+    public float jump, speed;
+    Vector2 force = new Vector2(0, 0);
+    Vector2 rAcceleration = new Vector2(0, 0);
+    bool bGrounded = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -13,21 +17,33 @@ public class RabbitController : MonoBehaviour
         rabbit = GetComponent<Rigidbody2D>();
     }
 
+
     // Update is called once per frame
     void Update()
     {
-        // movement keys
-        if (Input.GetKey("w"))
+        if ((Input.GetKey("w") || Input.GetKey(KeyCode.Space)) && bGrounded)
         {
-            rabbit.velocity = transform.up * jumpSpeed;
+            rabbit.velocity = transform.up * jump;
         }
-        if (Input.GetKey("a"))
+        if (Input.GetKey("a") && rabbit.velocity.x == 0)
         {
-            rabbit.velocity = transform.right * -maxSpeed;
+            rabbit.velocity += new Vector2(-speed, 0);
         }
-        if (Input.GetKey("d"))
+        if (Input.GetKey("d") && rabbit.velocity.x == 0)
         {
-            rabbit.velocity = transform.right * maxSpeed;
+            rabbit.velocity += new Vector2(speed, 0);
         }
+    }
+
+    // on collision = rabbit on ground
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        bGrounded = true;
+    }
+
+    // no collision = rabbit in air
+    void OnCollisionExit2D(Collision2D col)
+    {
+        bGrounded = false;
     }
 }
