12ecc7b5f4076458fe8b1f50d1bde094e4048efd
Veronica Hu <veronica.ss.hu@gmail.com>
2023-11-30 23:34:21 -0800

    Add main menu button to game over screen

--- SpaceRabbit/Assets/Scripts/MainMenu.cs
+++ SpaceRabbit/Assets/Scripts/MainMenu.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void ReturnMainMenu ()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Level1");
